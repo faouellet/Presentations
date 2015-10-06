14a8e3e207c1bc4fdab447399f6acdcdd2245e87
faouellet <faouellet@gmail.com>
2015-10-05 21:17:24 -0400

    Adding TMP version

---
+++ Functional/version2.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+template<size_t N, size_t Div3, size_t Div5>
+struct FizzBuzz
+{
+	static void print()
+	{
+		std::cout << N << std::endl;
+		FizzBuzz<N-1, (N-1)%3, (N-1)%5>::print();
+	}
+};
+
+template<size_t N>
+struct FizzBuzz<N, 0, 0>
+{
+	static void print()
+	{
+		std::cout << "FizzBuzz" << std::endl;
+		FizzBuzz<N-1, (N-1)%3, (N-1)%5>::print();
+	}
+};
+
+template<size_t N, size_t Div3>
+struct FizzBuzz<N, Div3, 0>
+{
+	static void print()
+	{
+		std::cout << "Buzz" << std::endl;
+		FizzBuzz<N-1, (N-1)%3, (N-1)%5>::print();
+	}
+};
+
+template<size_t N, size_t Div5>
+struct FizzBuzz<N, 0, Div5>
+{
+	static void print()
+	{
+		std::cout << "Fizz" << std::endl;
+		FizzBuzz<N-1, (N-1)%3, (N-1)%5>::print();
+	}
+};
+
+template<size_t Div3, size_t Div5>
+struct FizzBuzz<0, Div3, Div5>
+{
+	static void print()
+	{
+		std::cout << 0 << std::endl;
+	}
+};
+
+template<>
+struct FizzBuzz<0, 0, 0>
+{
+	static void print()
+	{
+		std::cout << 0 << std::endl;
+	}
+};
+
+template<size_t N>
+struct RunFizzBuzz
+{
+    static void print()
+    {
+        FizzBuzz<N, N%3, N%5>::print();
+    }
+};
+
+int main()
+{
+	RunFizzBuzz<100>::print(); 
+}

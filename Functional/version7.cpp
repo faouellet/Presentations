0cfe7207afa12b7a73170229b599f86d36e65712
faouellet <faouellet@gmail.com>
2015-10-12 09:38:00 -0400

    Adding a version with init capture

---
+++ Functional/version7.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <iterator>
+#include <type_traits>
+
+template <class T>
+class FibonacciIterator : public std::iterator<std::input_iterator_tag, T> {
+public:
+  FibonacciIterator() {
+    static_assert(std::is_integral<T>::value, "Integral type required");
+    mCurrent = 0;
+    mFirst = 0;
+    mSecond = 1;
+  }
+
+  T operator*() const { return mCurrent; }
+
+  FibonacciIterator &operator++() {
+    mCurrent = mFirst + mSecond;
+    mFirst = mSecond;
+    mSecond = mCurrent;
+    return *this;
+  }
+
+  FibonacciIterator &operator++(int) {
+    auto temp = *this;
+    operator++();
+    return *this;
+  }
+
+  bool operator==(const FibonacciIterator &fIt) const {
+    return mFirst == fIt.mFirst && mSecond == fIt.mSecond &&
+           mCurrent == fIt.mCurrent;
+  }
+
+  bool operator!=(const FibonacciIterator &fIt) const {
+    return !(*this == fIt);
+  }
+
+  bool operator<(const FibonacciIterator &fIt) const {
+    return mFirst < fIt.mFirst && mSecond < fIt.mSecond &&
+           mCurrent < fIt.mCurrent;
+  }
+
+private:
+  T mFirst;
+  T mSecond;
+  T mCurrent;
+};
+
+int main() {
+  FibonacciIterator<int> fib;
+  for (int i = 0; i < 10; ++i) {
+    std::cout << "Term " << i << " of the Fibonacci series: " << *(++fib)
+              << std::endl;
+  }
+}
